e4e761431ad6317b0190ec346545b9c4ee47adf2
XinruiChentw <xinrui.chen@thoughtworks.com>
2023-02-17 17:55:47 +0800

    delete unused variables

--- src/App.test.js
+++ src/App.test.js
@@ -7,8 +7,4 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn java link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn java/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
