265f5c4153a524b25b1c29db8e83734ed3c6ebfd
XinruiChentw <xinrui.chen@thoughtworks.com>
2023-02-17 15:47:32 +0800

    delete join

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,8 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+test('renders learn java link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn java/i);
+  expect(linkElement).toBeInTheDocument();
+});
